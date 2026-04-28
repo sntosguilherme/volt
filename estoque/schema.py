e83577fbfa850fdfe4bdae844ee099d758c22533
João Guilherme <sntos.guilherme9705@gmail.com>
2026-04-28 20:01:08 -0300

    feat: adição do schema e do  model

---
+++ estoque/schema.py
@@ -0,0 +1,21 @@
+from ninja import Schema
+from pydantic import Field
+from typing import Optional
+
+class MaterialIn(Schema):
+    nome: str
+    descricao: Optional[str] = None
+    quantidade_estoque: int = Field(default=0, gt=0) # restrição: A quantidade deve ser maior que 0.
+    cor: Optional[str] = None
+    unidade_medida: str
+    especificacao_tecnica: str
+
+class MaterialOut(Schema):
+    id: int
+    nome: str
+    descricao: Optional[str]
+    quantidade_estoque: int
+    cor: Optional[str]
+    unidade_medida: str
+    especificacao_tecnica: str
+    data_cadastro: str 
