58b13b7c79dbfb0748b944cc1112394cc761c391
João Guilherme <sntos.guilherme9705@gmail.com>
2026-04-28 23:49:47 -0300

    feat: implementação do post e do get

--- estoque/api.py
+++ estoque/api.py
@@ -1,5 +1,17 @@
 from ninja import NinjaAPI, Schema
 from typing import List
+from .schema import MaterialIn, MaterialOut
 from .models import Material
 
 api = NinjaAPI()
+
+#criar(create)
+@api.post("/materiais", response={201: MaterialOut})
+def criar_material(request, data: MaterialIn):
+    material = Material.objects.create(**data.dict())
+    return 201, material
+
+#ler(read)
+@api.get("/materiais", response=List[MaterialOut])
+def listar_materiais(request):
+    return Material.objects.all()
