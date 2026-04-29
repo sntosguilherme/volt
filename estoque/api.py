4b807e0a6ccab09de2c59c6835f16bbb2442220b
João Guilherme <sntos.guilherme9705@gmail.com>
2026-04-29 16:10:08 -0300

    feat: CRUD com rotas de PUT e DELETE

--- estoque/api.py
+++ estoque/api.py
@@ -1,7 +1,7 @@
 from ninja import NinjaAPI, Schema
-from typing import List
-from .schema import MaterialIn, MaterialOut
 from .models import Material
+from .schema import MaterialIn, MaterialOut
+from typing import List
 
 api = NinjaAPI()
 
@@ -15,3 +15,27 @@ def criar_material(request, data: MaterialIn):
 @api.get("/materiais", response=List[MaterialOut])
 def listar_materiais(request):
     return Material.objects.all()
+
+#atualizar(update)
+@api.put("/materiais/{material_id}", response=MaterialOut)
+def atualizar_material(request, material_id: int, data: MaterialIn):
+    try:
+        material = Material.objects.get(id=material_id)
+        for attr, value in data.dict().items():
+            setattr(material, attr, value)
+        material.save()
+        return material
+    except Material.DoesNotExist:
+        return 404, {"detail": "Material não encontrado"}
+    
+#deletar(delete)
+@api.delete("/materiais/{material_id}", response={204: None})
+def deletar_material(request, material_id: int):
+    try:
+        material = Material.objects.get(id=material_id)
+        material.delete()
+        return 204, None
+    except Material.DoesNotExist:
+        return 404, {"detail": "Não foi possível excluir: material inexistente."}
+    
+
